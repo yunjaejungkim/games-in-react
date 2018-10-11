ae730b2dd3c4442895863a4dc2047d372598f633
yunjae.jungkim <yunjae.jungkim@gmail.com>
2018-10-12 01:56:52 +0800

    Update paths

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const Hello = Loadable({
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/hello" component={Hello}/>
+      <Route exact path="/games-in-react" component={Home}/>
+      <Route path="/games-in-react/hello" component={Hello}/>
     </Switch>
   </Router>
 )
