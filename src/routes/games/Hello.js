6eb25dd975ac95b7f70500316b1417dcd4427e35
Truong Viet Trung <viettrung9012@yahoo.com>
2018-10-13 22:32:48 +0800

    Add Hello style (#7)
    * Update Home style
    
    * Add styles for Hello

--- src/routes/games/Hello.js
+++ src/routes/games/Hello.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Hello.css';
 
 const Hello = ({ name, updateName }) => (
-    <div>
+    <div id="hello">
+        <div class="home"><Link to ="/">Home</Link></div>
         <h2>Hello{name ? `, ${name}!` : ''}</h2>
         <label htmlFor="username">Username: </label>
-        <input type="text" name="username" onChange={(event) => { updateName(event.target.value); }} />
+        <input type="text" name="username" autoComplete="off" onChange={(event) => { updateName(event.target.value); }} />
     </div>
 );
 
